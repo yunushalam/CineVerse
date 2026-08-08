7b3c6fcf5e00ddc5f3819bb0f2e61e41e6be88c9
yunushalam <yunushalamalam32@gmai.com>
2026-08-09 01:32:21 +0530

    add CloudinaryService to handle file uploads via Cloudinary API

--- src/main/java/com/cineverse/api/service/CloudinaryService.java
+++ src/main/java/com/cineverse/api/service/CloudinaryService.java
@@ -20,7 +20,7 @@ public class CloudinaryService {
     }
 
     public String uploadFile(MultipartFile file) throws IOException {
-        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap("resource_type", "auto"));
         return uploadResult.get("secure_url").toString();
     }
 }
